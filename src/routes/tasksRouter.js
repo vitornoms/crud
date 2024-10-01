02aed0e832277528db79feb73396ba140dd42ba8
vitornoms <vitornoms@gmail.com>
2024-10-01 11:58:26 -0300

    certo

--- src/routes/tasksRouter.js
+++ src/routes/tasksRouter.js
@@ -7,9 +7,76 @@ const {
   deleteTask
 } = require('../controllers/tasksController')
 
+/**
+ * @swagger
+ * /tasks/list:
+ *  get:
+ *    summary: Retorna todas as tarefas
+ *    responses:
+ *      200:
+ *        description: Uma lista de tarefas
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ */
+
 router.get('/tasks/list', listTasks);
+
+/**
+ * @swagger
+ * /tasks/register:
+ *  post:
+ *    summary: Cadastra uma nova tarefa
+ *    responses:
+ *      200:
+ *        description: Sucesso!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ */
+
 router.post('/task/register', storeTask);
+
+/**
+ * @swagger
+ * /tasks/id:
+ *  put:
+ *    summary: Atualiza uma tarefa pelo ID
+ *    responses:
+ *      200:
+ *        description: Uma lista de tarefas
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ */
+
 router.put('/task/:id', updateTask);
+
+/**
+ * @swagger
+ * /tasks/delete:
+ *  delete:
+ *    summary: Remove uma tarefa pelo ID
+ *    responses:
+ *      200:
+ *        description: Uma lista de tarefas
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ */
+
 router.delete('/task/:id', deleteTask);
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,30 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const swaggerUi = require("swagger-ui-express")
+const swaggerJsDoc = require("swagger-jsdoc")
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API de Tarefas",
+            version: "1.0.0",
+            description: "API CRUD para gerenciar tarefas",
+        },
+        servers: [{ url: "http://localhost:3003"}],
+    },
+    apis: [`${__dirname}/routes/*.js`], // caminho para as rotas
+};
 
 const app = express()
 const port = process.env.PORT || 1903
+const taskRouter = require('./routes/tasksRouter');
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(express.json())
 app.use(cors())
-
-const taskRouter = require('./routes/tasksRouter')
-
 app.use('/api', taskRouter)
 
 app.listen(port, () => console.log(`Run on port ${port}!`))
